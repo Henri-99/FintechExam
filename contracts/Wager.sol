4be17a74f455f4b215acf772cedf03f2fc74c74c
Henri-99 <henriduplessis99@gmail.com>
2022-11-14 11:56:00 +0200

    basic ERC721 token

--- contracts/Wager.sol
+++ contracts/Wager.sol
@@ -1,13 +1,45 @@
+// SPDX-License-Identifier: GPL-3.0
 pragma solidity >=0.7.0 <0.9.0;
 
+import '@openzeppelin/contracts/token/ERC721/ERC721.sol';
+import '@openzeppelin/contracts/access/Ownable.sol';
+
 /**
- * @title Storage
- * @dev Store & retrieve value in a variable
+ * @title Wager
+ * @dev Mint token with encoded bet parameters
  * @custom:dev-run-script ./scripts/deploy_with_ethers.ts
  */
-contract Wager {
+contract Wager is ERC721, Ownable {
 
     uint256 number;
+
+    uint team;
+    uint odds;
+    
+    uint256 public wager = 0.05 ether;
+    uint256 public totalSupply;
+
+    address public bookmaker;
+
+    constructor() {
+        bookmaker = msg.sender;
+    }
+
+    /**
+     * @dev Set team selection at given odds with wager amount x
+     * @param selection is either 0/1 representing Team A or Team B in the bet
+     * @param multiplier is the odds multiplier in the event of a winning bet
+     * @param x is the amount of ETH wagered on the match
+     */
+    function bet(uint selection, uint multiplier, uint x) public {
+        team = selection;
+        odds = multiplier;
+        wager = x;
+    }
+
+    receive() payable external {
+        wager = msg.value;
+    }
 
     /**
      * @dev Store value in variable
